bda6f24c839937dbebe9f26a345392041161d269
Twin Panichsombat <roofimon@gmail.com>
2013-01-08 14:19:40 +0700

    first commit

---
+++ lib/srt_reader.rb
@@ -0,0 +1,3 @@
+class SrtReader
+
+end
---
+++ spec/srt_reader_spec.rb
@@ -0,0 +1,9 @@
+require	'spec_helper'
+require	'srt_reader'
+
+describe SrtReader do
+  context "read stl file and read it content into memory" do
+    it "should open STL file"
+    it "should recognize subtitle format like start_time, end_time, sub_string"
+  end
+end
