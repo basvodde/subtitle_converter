1ce458d9b6d0b555f2c8ecc5f6701a22d3f1d306
Twin Panichsombat <roofimon@gmail.com>
2013-01-09 15:47:34 +0700

    Refactor parse_stl_lines by extracting format method

--- lib/sub_rib_writer.rb
+++ lib/sub_rib_writer.rb
@@ -6,17 +6,15 @@ class SubRibWriter
   end
 
   def parse_stl_lines(stl_sub_title_lines)
-    line_number = 1
+    subtitle_number = 1
     stl_sub_title_lines.each do |line|
-      @subrib_sub_title_tokens = line.split("#")
-      @subrib_sub_title_lines.push(line_number.to_s+"\r"+
-                                   @subrib_sub_title_tokens[0].sub(/(...)$/){|s| ","+((33*s[1,2].to_i).to_s).rjust(3, "0") }+
-                                   "\t-->\t"+
-                                   @subrib_sub_title_tokens[1].sub(/(...)$/){|s| ","+((33*s[1,2].to_i).to_s).rjust(3, "0") }+
-                                   "\r"+
-                                   @subrib_sub_title_tokens[2]+"\r"
-      )
-      line_number = line_number+1
+      start_time, end_time, text_of_subtitle = line.split("#")
+      @subrib_sub_title_lines.push("#{subtitle_number}\r#{format(start_time)}\t-->\t#{format(end_time)}\r#{text_of_subtitle}\r")
+      subtitle_number = subtitle_number+1
     end
+  end
+
+  def format(original_format)
+    original_format.sub(/(...)$/){|s| ","+((33*s[1,2].to_i).to_s).rjust(3, "0") }
   end
 end
