4d07a33b40c25440046158e8e61ed7c778e7e73e
Twin Panichsombat <roofimon@gmail.com>
2013-01-09 16:00:11 +0700

    Refactor SubRibWriter by extracting convert_fps_to_millisec

--- lib/sub_rib_writer.rb
+++ lib/sub_rib_writer.rb
@@ -9,12 +9,16 @@ class SubRibWriter
     subtitle_number = 1
     stl_sub_title_lines.each do |line|
       start_time, end_time, text_of_subtitle = line.split("#")
-      @subrib_sub_title_lines.push("#{subtitle_number}\r#{format(start_time)}\t-->\t#{format(end_time)}\r#{text_of_subtitle}\r")
+      @subrib_sub_title_lines.push("#{subtitle_number}\r#{re_format(start_time)}\t-->\t#{re_format(end_time)}\r#{text_of_subtitle}\r")
       subtitle_number = subtitle_number+1
     end
   end
 
-  def format(original_format)
-    original_format.sub(/(...)$/){|s| ","+((33*s[1,2].to_i).to_s).rjust(3, "0") }
+  def re_format(hh_mm_ss_fps)
+    hh_mm_ss_fps.sub(/(...)$/){|fps| ","+convert_fps_to_millisec(fps)}
+  end
+
+  def convert_fps_to_millisec(frames_per_sec)
+    ((33*frames_per_sec[1,2].to_i).to_s).rjust(3, "0")
   end
 end
